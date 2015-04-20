d8ccf02ea68d0c5674d63fca54e9092b66021284
Jorge Saud <jorgelsaud@gmail.com>
2015-04-20 11:44:50 -0430

    add quinees somos page

--- resources/views/Paginas/inscripciones.blade.php
+++ resources/views/Paginas/inscripciones.blade.php
@@ -6,21 +6,21 @@
 				{!! wp_get_attachment_image($pagina->meta->imagen_beneficios,'imagenVertical') !!}
 			</div>
 			<div class="botonBanner">
-				<a href="">{!!$pagina->meta->texto_beneficios!!}</a></div>
+				<a href="{!!route('paginas',array('slug'=>'beneficio'))!!}">{!!$pagina->meta->texto_beneficios!!}</a></div>
 		</div>
 		<div class="col-xs-12 col-sm-4 text-center banner imagenHorizontal incsrpcionesBannerInterno">
 			<div class="contenedorImagen">
 				{!! wp_get_attachment_image($pagina->meta->imagen_requisitos,'imagenVertical') !!}
 			</div>
 			<div class="botonBanner">
-				<a href="">{!!$pagina->meta->texto_requisitos!!}</a></div>
+				<a href="{!!route('paginas',array('slug'=>'requisitos'))!!}">{!!$pagina->meta->texto_requisitos!!}</a></div>
 		</div>
 		<div class="col-xs-12 col-sm-4 text-center banner imagenHorizontal incsrpcionesBannerInterno">
 			<div class="contenedorImagen">
 				{!! wp_get_attachment_image($pagina->meta->imagen_solvencias,'imagenVertical') !!}
 			</div>
 			<div class="botonBanner">
-				<a href="">{!!$pagina->meta->texto_solvencias!!}</a></div>
+				<a href="{!!route('paginas',array('slug'=>'solvencias'))!!}">{!!$pagina->meta->texto_solvencias!!}</a></div>
 		</div>
 
 		<div class="clearfix"></div>
