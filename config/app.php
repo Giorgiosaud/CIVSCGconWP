360a489d0a83e77afc36f458ca772abbece06254
Jorge Saud <jorgelsaud@gmail.com>
2015-04-20 14:30:46 -0430

    cursos form

--- config/app.php
+++ config/app.php
@@ -145,7 +145,8 @@ return [
         'App\Providers\EventServiceProvider',
         'Barryvdh\Debugbar\ServiceProvider',
         'App\Providers\RouteServiceProvider',
-        'jorgelsaud\Carbonlocalizer\Provider\CarbonlocalizerServiceProvider'
+        'jorgelsaud\Carbonlocalizer\Provider\CarbonlocalizerServiceProvider',
+        'Bootstrapper\BootstrapperServiceProvider'
 
     ],
 
@@ -196,6 +197,30 @@ return [
         'View'      => 'Illuminate\Support\Facades\View',
         'Debugbar'  => 'Barryvdh\Debugbar\Facade',
         'CarbonLoc' => 'jorgelsaud\Carbonlocalizer\Facades\Carbonlocalizer',
+        'Accordion' => 'Bootstrapper\Facades\Accordion',
+        'Alert' => 'Bootstrapper\Facades\Alert',
+        'Badge' => 'Bootstrapper\Facades\Badge',
+        'Breadcrumb' => 'Bootstrapper\Facades\Breadcrumb',
+        'Button' => 'Bootstrapper\Facades\Button',
+        'ButtonGroup' => 'Bootstrapper\Facades\ButtonGroup',
+        'Carousel' => 'Bootstrapper\Facades\Carousel',
+        'ControlGroup' => 'Bootstrapper\Facades\ControlGroup',
+        'DropdownButton' => 'Bootstrapper\Facades\DropdownButton',
+        'Form' => 'Bootstrapper\Facades\Form',
+        'Helpers' => 'Bootstrapper\Facades\Helpers',
+        'Icon' => 'Bootstrapper\Facades\Icon',
+        'InputGroup' => 'Bootstrapper\Facades\InputGroup',
+        'Image' => 'Bootstrapper\Facades\Image',
+        'Label' => 'Bootstrapper\Facades\Label',
+        'MediaObject' => 'Bootstrapper\Facades\MediaObject',
+        'Modal' => 'Bootstrapper\Facades\Modal',
+        'Navbar' => 'Bootstrapper\Facades\Navbar',
+        'Navigation' => 'Bootstrapper\Facades\Navigation',
+        'Panel' => 'Bootstrapper\Facades\Panel',
+        'ProgressBar' => 'Bootstrapper\Facades\ProgressBar',
+        'Tabbable' => 'Bootstrapper\Facades\Tabbable',
+        'Table' => 'Bootstrapper\Facades\Table',
+        'Thumbnail' => 'Bootstrapper\Facades\Thumbnail',
 
     ],
 
