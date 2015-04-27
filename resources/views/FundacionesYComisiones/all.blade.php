301b76e57a949f38cecdc8cfc9be517e030011fe
Jorge Saud <jorgelsaud@gmail.com>
2015-04-27 11:43:10 -0430

    fundaciones

--- resources/views/FundacionesYComisiones/all.blade.php
+++ resources/views/FundacionesYComisiones/all.blade.php
@@ -3,7 +3,7 @@
 	<div class="container-fluid container-fluid-pegadito">
 		@foreach($Fundaciones as $i=>$fundacion)
 
-			<div class="col-xs-6 col-sm-4 col-md-3 text-center fundacionCuadrada"
+			<div class="col-xs-6 col-sm-4 text-center fundacionCuadrada"
 			     style="background-color: {!!$fundacion->meta->color_de_la_fundacion!!}">
 				<div class="nombreFundacion" style="color:{!!$fundacion->meta->color_del_texto!!}">
 					<a href="{!!route('fundacion',$fundacion->post_name)!!}">{!! $fundacion->post_title!!}</a>
