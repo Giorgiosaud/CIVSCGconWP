c54dbb67f2ad128d1f3327b9c049d19fe312ebbf
Jorge Saud <jorgelsaud@gmail.com>
2015-04-27 11:46:21 -0430

    sin cursos

---
+++ resources/views/Cursos/noHay.blade.php
@@ -0,0 +1,9 @@
+@extends('Plantillas.principal')
+@section('contenido')
+	@include('Cursos.extras.topImage')
+	@include('Cursos.extras.tags')
+		<div class="container-fluid container-fluid-pegadito cursos">
+			<h1>En Breve incluiremos los proximos cursos</h1>
+		</div>
+
+@endsection
