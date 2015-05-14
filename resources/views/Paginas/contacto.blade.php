a77b776552fc9138d8120e38ef3f07e087bfbd7c
Giorgiosaud <jorgelsaud@gmail.com>
2015-05-14 17:35:23 -0430

    contactanos

--- resources/views/Paginas/contacto.blade.php
+++ resources/views/Paginas/contacto.blade.php
@@ -1,12 +1,12 @@
 @extends('Plantillas.principal')
 @section('contenido')
-<div class="container-fluid">
+<div class="container">
     <ul class="list-group">
         @foreach ($errors->all() as $error)
             <li class="list-group-item list-group-item-danger">{{ $error }}</li>
         @endforeach
     </ul>
-
+    <h2 class="text-center">Contactanos</h2>
 
     {!!Form::horizontal()!!}
     {!!
