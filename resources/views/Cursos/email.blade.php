7cf959990dc5da7e801345fa03aae4940dbeb618
Giorgiosaud <jorgelsaud@gmail.com>
2015-05-19 17:46:03 -0430

    transfer

--- resources/views/Cursos/email.blade.php
+++ resources/views/Cursos/email.blade.php
@@ -1,7 +1,7 @@
-La Persona de nombre: {!!$nombre!!}
-Apellidos: {!!$apellido!!}
-Cedula : {!!$cedula!!}
-correo: {!!$correo!!}
-telefono: {!!$telefono!!}
-quiere hacer el curso {!!$nombreDeCurso!!}
+La Persona de nombre: {!!$nombre!!} <br/>
+Apellidos: {!!$apellido!!} <br/>
+Cedula : {!!$cedula!!} <br/>
+correo: {!!$correo!!} <br/>
+telefono: {!!$telefono!!} <br/>
+<strong>quiere hacer el curso {!!$nombreDeCurso!!} <br/></strong>
 el día {!!$fecha!!}
