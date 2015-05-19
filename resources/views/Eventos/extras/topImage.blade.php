8f57bdcc4a28d936e0e08d624c6b60857b6aecbb
Giorgiosaud <jorgelsaud@gmail.com>
2015-05-19 18:13:33 -0430

    cursos

--- resources/views/Eventos/extras/topImage.blade.php
+++ resources/views/Eventos/extras/topImage.blade.php
@@ -1,5 +1,5 @@
 <div class="container-fluid container-fluid-pegadito">
-	<div class="col-xs-12 text-center bannerInterno imagenVertical">
+	<div class="col-xs-12 text-center bannerInterno imagenHorizontal">
 		<div class="contenedorImagen">
 			<img
 					src="{!! get_theme_mod( 'imagen_area_eventos', 'Imagen Area de Eventos' ); !!}"
